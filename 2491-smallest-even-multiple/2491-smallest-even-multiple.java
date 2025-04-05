9b4c6f78661aedb6c2742263c3d33d2a7a0ca726
Chandranshu31 <rishpandey59@gmail.com>
2025-04-05 23:19:06 +0530

    Time: 0 ms (100%), Space: 40.5 MB (63.55%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.java
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.java
@@ -1,11 +1,11 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
         for(int i=2;i<Integer.MAX_VALUE;i++){
-            if(i%2==0 && i%n==0){
+            if(i%2==0 && i%n==0){  // from 2 to infinite return the very first number which is divisble by 2 and n
                 return i;
             }
         }
-        return 1;
+        return 1; // must nvr reaach here
         
     }
 }
