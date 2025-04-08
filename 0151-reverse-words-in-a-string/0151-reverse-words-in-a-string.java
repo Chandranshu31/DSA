446132873e6fac6e539e4ca8544db540df07b159
Chandranshu31 <rishpandey59@gmail.com>
2025-04-08 10:58:30 +0530

    Time: 6 ms (86.26%), Space: 43 MB (80.37%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -3,15 +3,17 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         String words[]= s.trim().split("\\s+");
 
-        for(int i=words.length-1;i>=0;i--){
+        for(int i=words.length-1;i>=0;i--){  // start storing from the end so sb will have the asn in reverse order only
             sb.append(words[i]);
             if(i!=0){
-                sb.append(" ");
+                sb.append(" "); // add whitespace but not before the first word
             }
         }
         return sb.toString();
 
-
+// with the help of trim we removed the leading and trailin spaces and with the help of split("\\s+") we converted our
+// string into an array and all the whitespaces,tab were ignored 
+// therefore we get words[]=["the","sky","is","blue"] and ["hello","world"];
         
     }
 }
