9ebd1a07bc4b147b20fba5702f39ec5da0b3583c
Chandranshu31 <rishpandey59@gmail.com>
2025-04-08 23:54:50 +0530

    Time: 6 ms (86.26%), Space: 43.1 MB (71.14%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -3,19 +3,52 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         String words[]= s.trim().split("\\s+");
 
-        for(int i=words.length-1;i>=0;i--){  // start storing from the end so sb will have the asn in reverse order only
+        for(int i=words.length-1;i>=0;i--){
             sb.append(words[i]);
             if(i!=0){
-                sb.append(" "); // add whitespace but not before the first word
+                sb.append(" ");
             }
         }
         return sb.toString();
 
-// with the help of trim we removed the leading and trailin spaces and with the help of split("\\s+") we converted our
-// string into an array and all the whitespaces,tab were ignored 
-// therefore we get words[]=["the","sky","is","blue"] and ["hello","world"];
+
         
     }
 }
 
+/**
+An alternate solution withput using any kind of inbuilt function
+class Solution {
+    public String reverseWords(String s) {
+        Stack<String> st = new Stack<>();
+        int i=0;
+        int n= s.length();
 
+        while(i<n){ // until we iteraate the whole sentence which is in form of string 
+            while(i<n && s.charAt(i)==' '){  // to skip the leading and trailing and mid spaces 
+                i++;
+            }
+            String word=""; // to get every word until a white space ocurs whch marks the end of the single word
+            while(i<n && s.charAt(i)!=' ')
+            {
+                word+=s.charAt(i);
+                i++;
+            }
+            if(word.length()!=0){  // add that word into the stack
+                st.push(word);
+            }
+
+        }
+        String result=""; // time to make the rsultant string 
+        while(!st.isEmpty()){
+            result+=st.pop();  // keep adding every word
+            if(!st.isEmpty()){  //  to add whitespace between two words and not before the last word 
+                result+=" ";
+            }
+        }
+
+        return result; 
+
+        
+    }
+}  to do it withpout using any inbuilt function  */
