d1fecb47e4bcf03784d73c121e9c2e2ba5d91c27
Chandranshu31 <rishpandey59@gmail.com>
2025-04-12 18:08:15 +0530

    Time: 2 ms (95.04%), Space: 133.7 MB (80.93%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -3,7 +3,7 @@ class Solution {
         int first=Integer.MAX_VALUE;
         int second = Integer.MAX_VALUE;
         int n= nums.length;
-
+           // the idea is to get the third greater element after first and second if we could not then ret false
         for(int i=0;i<n;i++){
             if(nums[i]<=first){
                 first=nums[i];
@@ -19,3 +19,71 @@ class Solution {
         
     }
 }
+
+/**
+These 2 approaches I used when I did not know that it is not necessary to have contagious sequence and 2nd one is correct till
+an extent but too much greedy so fails on few test cases
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int k=3;  // the size of our window
+        int n=nums.length;
+        if(n<k){
+            return false;
+        }
+         boolean isIncTrip=false;
+        for(int i=0;i<=n-k;i++){
+            int p=i;
+            int j=i+k-1;
+            boolean isInc=true;
+            while(j>i){
+                if(nums[j-1]>=nums[j]){
+                    isInc=false;
+                }
+                j--;
+            }
+            if(isInc){
+                isIncTrip=true;
+                break;
+            }
+
+        }
+        return isIncTrip;
+        
+    }
+}
+
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+        int k = 3;
+        if (n < k) {
+            return false;
+        }
+
+        boolean isIncTrip = false;
+        for (int i = 0; i <= n - k; i++) {
+            int curr = nums[i];
+            int count = 0;
+            for (int j = i + 1; j < n; j++) {
+                if (count == 0 && nums[j] > curr) {
+                    count++;  // We found the first larger number
+                    curr = nums[j];  // Update curr to this number
+                } else if (count == 1 && nums[j] > curr) {
+                    count++;  // We found the second larger number
+                    break;  // Exit the loop since we found the triplet
+                }
+            }
+            if (count == 2) {
+                isIncTrip = true;
+                break;  // Found the triplet, break the outer loop
+            }
+        }
+        return isIncTrip;
+    }
+}
+
+
+
+
+
+ */
