2e681fea21f1d2ff9645e6836509b6051c8760db
Chandranshu31 <rishpandey59@gmail.com>
2025-04-12 18:09:47 +0530

    Time: 2 ms (95.04%), Space: 133.9 MB (62.52%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,22 +1,20 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        int first=Integer.MAX_VALUE;
-        int second = Integer.MAX_VALUE;
-        int n= nums.length;
-           // the idea is to get the third greater element after first and second if we could not then ret false
-        for(int i=0;i<n;i++){
-            if(nums[i]<=first){
-                first=nums[i];
-            }
-            else if(nums[i]<=second){
-                second=nums[i];
-            }
-            else{
-                return true;
+        int firstSmallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+        int n = nums.length;
+        // the idea is to get the third greater element after first and second if we could not then ret false
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= firstSmallest) {
+                firstSmallest = nums[i];
+            } else if (nums[i] <= secondSmallest) {
+                secondSmallest = nums[i];
+            } else {
+                return true; // if a numb is not less than first smallest and seocnd smallest tehn it means its bigger than both of them and hence we got our triplet
             }
         }
         return false;
-        
+
     }
 }
 
