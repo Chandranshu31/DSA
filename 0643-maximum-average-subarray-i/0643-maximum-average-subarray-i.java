044282df7354708d58c6105c7c62e3009762060b
Chandranshu31 <rishpandey59@gmail.com>
2025-04-14 12:36:56 +0530

    Time: 2 ms (99.87%), Space: 55.5 MB (96.16%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -12,12 +12,12 @@ class Solution {
 
         for(int i=0;i<k;i++){
             windowSum+=nums[i];
-        }
+        }  // for first window of sze k
       
         maxSum=windowSum;
         // to get the max avg we need max sum window of size k
         for(int i=k;i<n;i++){
-            windowSum+=nums[i]-nums[i-k];
+            windowSum+=nums[i]-nums[i-k]; // slide window 1 step ahead and keep removing a element from back, one at a time
             maxSum=Math.max(maxSum,windowSum);
         }
 
