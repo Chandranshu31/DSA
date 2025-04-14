3c60fa9376d962fa979996b74bf30f81fb4cb324
Chandranshu31 <rishpandey59@gmail.com>
2025-04-14 11:34:10 +0530

    Time: 12 ms (82.33%), Space: 41.4 MB (88.83%) - LeetHub

--- 2122-count-special-quadruplets/2122-count-special-quadruplets.java
+++ 2122-count-special-quadruplets/2122-count-special-quadruplets.java
@@ -1,12 +1,12 @@
 class Solution {
     public int countQuadruplets(int[] nums) {
         int n=nums.length;
-        if(n<4){
+        if(n<4){   // need atleast 4 elements to form a quadruplets
             return 0;
         }
         int count=0;
 
-        for(int i=0;i<=n-4;i++){
+        for(int i=0;i<=n-4;i++){  // <= n-4 to form a valid quadruplet ahead
             for(int j=i+1;j<=n-3;j++){
                 for(int k=j+1;k<=n-2;k++){
                     for(int z=k+1;z<n;z++){
