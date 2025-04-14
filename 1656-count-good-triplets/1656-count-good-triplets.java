3eb1811f8b62b615622cd1863950548ebc8d570e
Chandranshu31 <rishpandey59@gmail.com>
2025-04-14 11:29:44 +0530

    Time: 10 ms (77.35%), Space: 41.5 MB (45.6%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.java
+++ 1656-count-good-triplets/1656-count-good-triplets.java
@@ -1,20 +1,26 @@
 class Solution {
     public int countGoodTriplets(int[] arr, int a, int b, int c) {
-        int n=arr.length;
-        int count=0;
-        if(n<3){
+        int n = arr.length;
+        int count = 0;
+        if (n < 3) {  // we atleast need 3 elements to have triplets
             return 0;
         }
-       for(int i=0;i<=n-3;i++){
-               for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                if(Math.abs(arr[i]-arr[j])<=a && Math.abs(arr[j]-arr[k])<=b && Math.abs(arr[k]-arr[i])<=c){
-            count++;
+        for (int i = 0; i <= n - 3; i++) {
+            for (int j = i + 1; j <= n - 2; j++) {
+                if (Math.abs(arr[i] - arr[j]) <= a) {
+                    for (int k = j + 1; k <=n-1; k++) {
+                        if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[k] - arr[i]) <= c) {
+                            count++;
+                        }
+                    }
                 }
-               }
-       }
-       }
-    
+            }
+        }
+
         return count;
     }
-    }
+}
+// the basic approach would be to run a loop form i to n,jton,kton and then check wether condition satiesfies
+// a bit improved would be to itrate i from 0 to<=n-3, j to<=n-2 and k to <=n-1 and then check condition
+// a bit more improved would be to check first condition before enetering into the third loop
+// and then check the rest conditions
