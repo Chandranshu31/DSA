6eecb11d4e5fe58c014a0f34484e9db934b369b3
Chandranshu31 <rishpandey59@gmail.com>
2025-04-19 12:36:08 +0530

    Time: 13 ms (71.8%), Space: 44.8 MB (92.68%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -36,3 +36,45 @@ class Solution {
         
     }
 }
+
+/** THIS SOLUTION IS ALSO  THE SAME BUT IT WILL TAKE A BIT MORE PROCESSING TIME THAN ABOVE SOLN BECAUSE 
+HERE WE R USING STRING WE CREATES A NEW OBJECT REF EVERYTIME WE DO RES+= OPERATION UNLIKE THE SB, WHICH UPDATES THE MEMORY
+BUFFER ONLY
+class Solution {
+    public String frequencySort(String s) {
+        int n=s.length();
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        pq.addAll(map.values());
+         String result ="";
+         while(!pq.isEmpty()){
+            int freq=pq.remove(); // get the highest frequency
+            // now find the character with this frequency in map which stores character and their frequencies
+            for(Map.Entry<Character,Integer> entry: map.entrySet()){
+                int currFreq= entry.getValue();
+                if(currFreq==freq){ // get the character only if the xurr freq is equal to maxfreq
+                    char ch=entry.getKey();
+                    for(int i=0;i<freq;i++){
+                        result+=ch;  // append the character to result for freq times
+                    }
+                      map.put(ch,0);  // reduce the freq to zero
+                      break;
+                }
+              
+            }
+         }
+
+         return result;
+
+
+
+        
+        
+    }
+} */
