cb6ac9f7536b71b80a30a1950565febcb35b8f6f
Chandranshu31 <rishpandey59@gmail.com>
2025-04-24 11:31:55 +0530

    Time: 80 ms (21.51%), Space: 48.9 MB (98.8%) - LeetHub

--- 2378-sender-with-largest-word-count/2378-sender-with-largest-word-count.java
+++ 2378-sender-with-largest-word-count/2378-sender-with-largest-word-count.java
@@ -1,16 +1,16 @@
 class Solution {
     public String largestWordCount(String[] messages, String[] senders) {
-        TreeMap <String,Integer>  map = new TreeMap<>(Collections.reverseOrder());
+        TreeMap <String,Integer>  map = new TreeMap<>(Collections.reverseOrder());  // to get the lexographically larger names at top so the momnet we match the highest freq we return it in case of same freq count
         for(int i=0;i<messages.length;i++){
             String name=senders[i];
             String message=messages[i];
-            int count=1;
+            int count=1;  // to count the number of words the string has
             for(int j=0;j<message.length();j++){
-                if(message.charAt(j)==' '){
+                if(message.charAt(j)==' '){ //  B C D, initllay count=1... 3 spaces--count=4 and thats the final also
                     count++;
                 }
             }
-            map.put(name,map.getOrDefault(name,0)+count);
+            map.put(name,map.getOrDefault(name,0)+count); // map the name with the total word count they have sent
         }
         int highestFreq=Integer.MIN_VALUE;
 
