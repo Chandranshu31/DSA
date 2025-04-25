2047350636e0c1b16c538ac5f777c857d4b9faff
Chandranshu31 <rishpandey59@gmail.com>
2025-04-25 14:31:39 +0530

    Time: 12 ms (6.58%), Space: 42.5 MB (19.79%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.java
+++ 1217-relative-sort-array/1217-relative-sort-array.java
@@ -2,27 +2,27 @@ class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
         int n=arr1.length;
         List<Integer> list= new ArrayList<>();
-       // LinkedHashSet<Integer> set = new LinkedHashSet<>(); // to get the relative order
-       TreeMap<Integer,Integer> map = new TreeMap<>();
+       
+       TreeMap<Integer,Integer> map = new TreeMap<>(); // so that the values of arr1 get stored in ascending order as akey ad we dont need to sort the left overs
        for(int i=0;i<n;i++){
-        map.put(arr1[i],map.getOrDefault(arr1[i],0)+1);
+        map.put(arr1[i],map.getOrDefault(arr1[i],0)+1);  // put the elements of arr1 in map with their respective frequencies
        }
 
-       for(int i=0;i<arr2.length;i++){
+       for(int i=0;i<arr2.length;i++){ 
         int curr=arr2[i];
-        for(int key: map.keySet()){
-            if(curr==key){
-                int freq=map.get(key);
-                for(int j=0;j<freq;j++){
+        for(int key: map.keySet()){  // given that all the elements of a2 exits in a1
+            if(curr==key){  // so this never fails, get the order
+                int freq=map.get(key);  // get the freq 
+                for(int j=0;j<freq;j++){  // fill the list for that much time
                     list.add(key);
                 }
             }
         }
-        map.remove(curr);
+        map.remove(curr);  // keep removing the key after it got filled in list
        }
 
-    if(list.size()!=arr1.length){
-          for(int key: map.keySet()){
+    if(list.size()!=arr1.length){  // if there are leftovers, i.e elements in a1 that r not in a2 then 
+          for(int key: map.keySet()){  // the keys r aslready sorted in asceding order, so just pick them up form the map and fill it in the list
                 int freq=map.get(key);
                 for(int j=0;j<freq;j++){
                     list.add(key);
