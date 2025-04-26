7a28433a56d6968f9b4e3ed855a3874c0bfef785
Chandranshu31 <rishpandey59@gmail.com>
2025-04-27 01:11:55 +0530

    Time: 15 ms (13.41%), Space: 64.2 MB (8.86%) - LeetHub

--- 2634-minimum-common-value/2634-minimum-common-value.java
+++ 2634-minimum-common-value/2634-minimum-common-value.java
@@ -14,3 +14,28 @@ class Solution {
         
     }
 }
+
+/**
+ANOTHER APPROACH USING TREEMAP(sorted map)
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        TreeMap<Integer, Integer> m1 = new TreeMap<>();
+        TreeMap<Integer,Integer> m2 = new TreeMap<>();
+
+        for(int num:nums1){
+            m1.put(num,m1.getOrDefault(num,0)+1);
+        }
+         for(int num:nums2){
+            m2.put(num,m2.getOrDefault(num,0)+1);
+        }
+
+        for(int key: m2.keySet()){
+            if(m1.containsKey(key)){
+                return key;
+            }
+        }
+        return -1;
+        
+    }
+}
+ */
