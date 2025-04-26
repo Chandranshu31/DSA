b2b9a6cd35cfbe5f18cf320bbf0ec26c984d12ca
Chandranshu31 <rishpandey59@gmail.com>
2025-04-27 01:11:20 +0530

    Time: 18 ms (7.57%), Space: 64.2 MB (8.86%) - LeetHub

--- 2634-minimum-common-value/2634-minimum-common-value.java
+++ 2634-minimum-common-value/2634-minimum-common-value.java
@@ -1,21 +1,16 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        TreeMap<Integer, Integer> m1 = new TreeMap<>();
-        TreeMap<Integer,Integer> m2 = new TreeMap<>();
+      HashSet<Integer> set = new HashSet<>();
+      for(int num: nums1){
+        set.add(num);
+      }
 
-        for(int num:nums1){
-            m1.put(num,m1.getOrDefault(num,0)+1);
+      for(int num:nums2){
+        if(set.contains(num)){
+            return num;
         }
-         for(int num:nums2){
-            m2.put(num,m2.getOrDefault(num,0)+1);
-        }
-
-        for(int key: m2.keySet()){
-            if(m1.containsKey(key)){
-                return key;
-            }
-        }
-        return -1;
+      }
+      return -1;
         
     }
 }
