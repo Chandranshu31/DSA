1963223e1e4c75e0245ffeae7b493b580e8185e6
Chandranshu31 <rishpandey59@gmail.com>
2025-04-29 00:02:39 +0530

    Time: 33 ms (76.16%), Space: 57.4 MB (84.2%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -7,15 +7,15 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
 
         for(int right=0;right<n;right++){
-            sum+=nums[right];
-            while(set.contains(nums[right])){
+            sum+=nums[right]; // expand the window and keep adding the elemnt into sum
+            while(set.contains(nums[right])){  // if the curr elment exists in set then start shrinking the window by removing left and try to make the window of distinct
                 sum-=nums[left];
                 set.remove(nums[left]);
                 left++;
             }
-            set.add(nums[right]);
+            set.add(nums[right]); // add the curr into set
 
-            if(right-left+1==k){
+            if(right-left+1==k){  // if window size reached then update and the maxsum and shrink the window(right will expand it) and look for next subarray of same condition with k length and calc its sum
                 maxSum=Math.max(sum,maxSum);
                 sum-=nums[left];
                 set.remove(nums[left]);
