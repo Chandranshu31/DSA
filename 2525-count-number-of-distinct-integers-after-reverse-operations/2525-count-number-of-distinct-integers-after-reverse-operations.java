9112d2027e0ab452078bfd47de52efdc558ad53d
Chandranshu31 <rishpandey59@gmail.com>
2025-05-06 01:15:23 +0530

    Time: 67 ms (28%), Space: 63.3 MB (85.51%) - LeetHub

---
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/2525-count-number-of-distinct-integers-after-reverse-operations.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int countDistinctIntegers(int[] nums) {
+        int n=nums.length;
+       HashSet<Integer> set = new HashSet<>();
+       for(int num:nums){
+        set.add(num);
+       }
+
+        for(int i=0;i<n;i++){
+            int r=rev(nums[i]);
+            set.add(r);
+        }
+        return set.size();
+        
+    }
+    public int rev(int num){
+        long res=0;
+        while(num!=0){
+            int lastDigit=num%10;
+            res=res*10+lastDigit;
+            if(res > Integer.MAX_VALUE){ // to control the overflow of integer
+                return 0;}
+            if(res < Integer.MIN_VALUE){//same as above
+                return 0;
+            }
+            num/=10;
+        }
+        return(int)res;
+        
+    }
+}
