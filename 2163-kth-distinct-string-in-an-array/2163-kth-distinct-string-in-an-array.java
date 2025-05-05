6750d91eeb2a685041c2205c240385c91017741a
Chandranshu31 <rishpandey59@gmail.com>
2025-05-06 01:49:53 +0530

    Time: 5 ms (99.43%), Space: 44.5 MB (31.87%) - LeetHub

--- 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.java
+++ 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.java
@@ -4,19 +4,20 @@ class Solution {
         HashSet<String> duplicates = new HashSet<>();
         for(String el:arr){
             if (duplicates.contains(el)) {
-                continue; // already known as duplicate
+                continue; // already known as duplicate do nothing n skip this iteration
             }
             if(map.containsKey(el)){
-                map.remove(el);
-                duplicates.add(el);
+                map.remove(el);  // if an el appeared for more than 1 time just remove it from ur map
+                duplicates.add(el); // add to duplicate
             }else{
             map.put(el,1);
             }
         }
+        // now map only has elements with freq==1 in arr and the insertion order is maintained due to linkedhashmap
         if(map.size()<k){
             return "";
         }
-        int i = 0;
+        int i = 0; // jus get the kth key
         for (String key : map.keySet()) {
             i++;
             if (i == k) return key;
