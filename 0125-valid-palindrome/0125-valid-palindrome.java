7e83eeaa45a0351ed06e4e79563c715d96fee8db
Chandranshu31 <rishpandey59@gmail.com>
2025-05-06 23:13:03 +0530

    Time: 2 ms (99.03%), Space: 43.2 MB (69.77%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int n=s.length();
+        if(n==0){
+            return true;
+        }
+        int start=0;
+        int end=n-1;
+
+        while(start<end){
+            char currFirst=s.charAt(start);
+            char currLast=s.charAt(end);
+            if(!Character.isLetterOrDigit(currFirst)){
+                start++;
+            }
+            else if(!Character.isLetterOrDigit(currLast)){
+                end--;
+            }
+            else{
+                if(Character.toLowerCase(currFirst)!=Character.toLowerCase(currLast)){
+                    return false;
+                }
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+}
