48ff7bbfb7a7111686ec77e5aec65733d4dd1a2c
Chandranshu31 <rishpandey59@gmail.com>
2025-05-17 13:22:37 +0530

    Time: 5 ms (94.77%), Space: 42.5 MB (63.76%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
@@ -22,3 +22,32 @@ class Solution {
        return remaining;
     }
 }
+
+/**
+In above solution i calculated remaining hand on hand by keep updating it but we can also calculate it at the end 
+of the code like this
+class Solution {
+    public long pickGifts(int[] gifts, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int j=0;j<gifts.length;j++){
+            pq.add(gifts[j]);
+        }
+
+       for(int i=0;i<k;i++){ 
+        if(!pq.isEmpty()){
+       
+        int mostFreq=pq.remove(); 
+        double sqrtVal=Math.sqrt(mostFreq);
+        int floorVal=(int)Math.floor(sqrtVal);
+        pq.add(floorVal);
+        }
+       }
+
+       long remaining=0;
+       while(!pq.isEmpty()){
+        remaining+=pq.remove();
+       } 
+       return remaining;
+    }
+}
+ */
