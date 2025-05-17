03637eba9b302494a86e1f60f501fedacc12f79d
Chandranshu31 <rishpandey59@gmail.com>
2025-05-17 13:21:24 +0530

    Time: 5 ms (94.77%), Space: 42.8 MB (25.17%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
@@ -1,8 +1,11 @@
 class Solution {
     public long pickGifts(int[] gifts, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        long remaining=0;
+
         for(int j=0;j<gifts.length;j++){
             pq.add(gifts[j]);
+            remaining+=gifts[j];
         }
 
        for(int i=0;i<k;i++){ 
@@ -12,13 +15,10 @@ class Solution {
         double sqrtVal=Math.sqrt(mostFreq);
         int floorVal=(int)Math.floor(sqrtVal);
         pq.add(floorVal);
+        remaining-=mostFreq;
+        remaining+=floorVal;
         }
        }
-
-       long remaining=0;
-       while(!pq.isEmpty()){
-        remaining+=pq.remove();
-       } 
        return remaining;
     }
 }
