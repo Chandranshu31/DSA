f04159631d166cf81d97f7cc2de866218fa0aa69
Chandranshu31 <rishpandey59@gmail.com>
2025-05-21 11:47:47 +0530

    Time: 7 ms (92.7%), Space: 60.7 MB (56.58%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.java
@@ -41,3 +41,38 @@ class Solution {
 so we set a pointer prev to track the current min among the group of adjacent same elements
  if we get a prev with smaller val then remove it by adding its cost and updating the prev
  or else just get the current one as minn*/
+
+ /**
+ 
+public class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        int totalCost = 0;
+        int n = colors.length();
+        int i = 0;
+
+        while (i < n - 1) { 
+            if (colors.charAt(i) == colors.charAt(i + 1)) {
+                List<Integer> group = new ArrayList<>();
+                group.add(neededTime[i]);
+
+                int j = i + 1;
+                while (j < n && colors.charAt(j) == colors.charAt(i)) {
+                    group.add(neededTime[j]);
+                    j++;
+                } 
+
+                Collections.sort(group);
+                for (int k = 0; k < group.size() - 1; k++) {
+                    totalCost += group.get(k);
+                }
+
+                i = j; // set the pointer i to the next color
+            } else {
+                i++;
+            }
+        }
+
+        return totalCost;
+    }
+}
+  ANOTHER VALID SOLUTION */
