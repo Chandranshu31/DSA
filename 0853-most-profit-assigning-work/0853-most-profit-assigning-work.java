fae81db765f282bf211ba9162154e8149710c13e
Chandranshu31 <rishpandey59@gmail.com>
2025-05-22 16:37:18 +0530

    Time: 515 ms (11.13%), Space: 45.7 MB (67.86%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.java
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.java
@@ -1,21 +1,25 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        int matrix[][]= new int[profit.length][2];
+        int matrix[][]=new int[difficulty.length][2];
         for(int i=0;i<matrix.length;i++){
             matrix[i][0]=difficulty[i];
             matrix[i][1]=profit[i];
-        } 
+        }
+
+        Arrays.sort(matrix,Comparator.comparingInt(o -> o[0]));
+        Arrays.sort(worker);
         int maxProfit=0;
-        Arrays.sort(matrix,Comparator.comparingDouble(o -> o[0]));
         for(int i=0;i<worker.length;i++){
             int currCapacity=worker[i];
-            int best = 0;
-            for (int j = 0; j < matrix.length && matrix[j][0] <= currCapacity; j++) {
-                best = Math.max(best, matrix[j][1]);
+            int best=0;
+            for(int j=0;j<matrix.length&& matrix[j][0]<=currCapacity;j++){
+                best=Math.max(best,matrix[j][1]);
             }
-            maxProfit += best;
+            maxProfit+=best;
         }
+        
         return maxProfit;
-
     }
 }
+
+// sort the matrix on basis of difficulty so that we can get the 
