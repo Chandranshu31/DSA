49c9d918ffd60fac61e9ba7e74e23269e40be3f6
Chandranshu31 <rishpandey59@gmail.com>
2025-05-22 23:20:26 +0530

    Time: 109 ms (13.68%), Space: 73.6 MB (11.79%) - LeetHub

--- 3351-maximize-happiness-of-selected-children/3351-maximize-happiness-of-selected-children.java
+++ 3351-maximize-happiness-of-selected-children/3351-maximize-happiness-of-selected-children.java
@@ -25,3 +25,36 @@ class Solution {
       
 }
 }
+
+/**
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        // we will always consider to pick the child with maximum happiness, to maximize our sum
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int h: happiness){
+            pq.add(h);
+        }
+        long maxHappiness=0;
+        
+
+        for(int i=0;i<k;i++){
+            if(pq.isEmpty()) break;
+
+            int currMax=pq.remove();
+            maxHappiness+=currMax;
+              // now we need to decrement the other happinesses by 1 and we cant directly modify yhe pq
+              // so make a temp pq in which add only those values which are >0 after decreasing by1
+            PriorityQueue<Integer> temp = new PriorityQueue<>(Collections.reverseOrder());
+            while(!pq.isEmpty()){  // 
+                int val=pq.remove()-1;
+                if(val>0){
+                    temp.add(val);
+                }
+            }
+            pq=temp;  // this is how we copy or assign all the values of a pq into the other pq
+        }
+        return maxHappiness;
+
+  ANOTHER LOGIC BUT TLE     
+}
+} */
