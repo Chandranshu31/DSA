57e379230f04c966bb3b594a53bd25c5193cc849
Chandranshu31 <rishpandey59@gmail.com>
2025-05-23 11:41:37 +0530

    Time: 40 ms (82.96%), Space: 87.8 MB (38.52%) - LeetHub

--- 2388-replace-elements-in-an-array/2388-replace-elements-in-an-array.java
+++ 2388-replace-elements-in-an-array/2388-replace-elements-in-an-array.java
@@ -20,3 +20,32 @@ class Solution {
     }
 
 }
+
+/**
+Another approach but good for small cases only
+class Solution {
+    public int[] arrayChange(int[] nums, int[][] operations) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(i,nums[i]);
+        }
+
+        int n=operations.length;
+        for(int i=0;i<n;i++){
+            int a=operations[i][0]; // it exists in nums
+            int b=operations[i][1]; // the val with which we want it to replace
+            for(int k: map.keySet()){
+                if(map.get(k)==a){
+                    map.put(k,b);
+                }
+            }
+        }
+
+        int res[]=new int[nums.length];
+        for(int k:map.keySet()){
+            res[k]=map.get(k);
+        }
+        return res;
+    }
+}
+ */
