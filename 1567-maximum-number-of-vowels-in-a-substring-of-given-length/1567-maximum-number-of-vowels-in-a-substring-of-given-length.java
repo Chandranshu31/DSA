79f22dd99d289bfc523971c03252d6bb18516a31
Chandranshu31 <rishpandey59@gmail.com>
2025-05-24 23:02:22 +0530

    Time: 10 ms (91.9%), Space: 44.9 MB (44.18%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -2,29 +2,27 @@ class Solution {
     public int maxVowels(String s, int k) {
         int maxNumb=0;
         int n=s.length();
-        for(int i=0;i<k;i++){
+        for(int i=0;i<k;i++){   // check  the number of vowels in the first window, and make it the curr maxnumb
             if(isVowel(s.charAt(i))){
                 maxNumb++;
             }
         }
-        int count=maxNumb;
+        int count=maxNumb; // asssign the curr count as maxnumb
         for(int i=k;i<n;i++){
-            if(isVowel(s.charAt(i-k))){
+            if(isVowel(s.charAt(i-k))){  // is the char going out of this window ws vowel decrease the count , currcount
                 count--;
             }
-            if(isVowel(s.charAt(i))){
+            if(isVowel(s.charAt(i))){  // if the element coming in window is vowel increment the count
                 count++;
             }
-            maxNumb=Math.max(count,maxNumb);
+            maxNumb=Math.max(count,maxNumb);  // keep updating the maxnumb after every iteration
 
         }
 
         return maxNumb;
-
-        
     }
 
-    public boolean isVowel(char ch){
+    public boolean isVowel(char ch){  // fucn to check if a char is vowel or not
         if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
             return true;
         }
