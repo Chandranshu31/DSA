8bca3aab45afc96ef547ec01a13052a8017fa042
Chandranshu31 <rishpandey59@gmail.com>
2025-05-27 11:07:07 +0530

    Time: 27 ms (88.82%), Space: 57.5 MB (72.06%) - LeetHub

---
+++ 2655-rearrange-array-to-maximize-prefix-score/2655-rearrange-array-to-maximize-prefix-score.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxScore(int[] nums) {
+        // we want more numbers in prefix array to be positive 
+        // so it would be best for us if we could club the all of the positive numbers together and in front of the array
+        /// cause of  the prefix.
+        Arrays.sort(nums);  // sort in asc
+        int a=0;
+        int b=nums.length-1;
+        while(a<b){
+            int temp=nums[a];
+            nums[a]=nums[b];
+            nums[b]=temp;
+            a++;
+            b--;
+        }   // no array is sorted in descending order
+
+        int maxLength=0;
+        long prefixSum=0;
+        for(int i=0;i<nums.length;i++){
+            prefixSum+=nums[i];
+            if(prefixSum>0){
+                maxLength++;
+            }else{
+                break;
+            }
+        }
+
+        return maxLength;
+
+        
+    }
+}
