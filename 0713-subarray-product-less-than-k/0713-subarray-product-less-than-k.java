764017065479425fc950bccd62134b4efbc5908f
Chandranshu <rishpandey59@gmail.com>
2025-05-31 15:39:14 +0530

    Time: 4 ms (99.98%), Space: 47.6 MB (81.59%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -5,11 +5,13 @@ class Solution {
         int count=0;
         int prod=1;
         while(j<nums.length){
-            prod*=nums[j];
-            while(prod>=k && i<=j){
+            prod*=nums[j];  // add current pointer el to prod
+
+            while(prod>=k && i<=j){  // keep shrinking the window till we are getting the prod >=k by removing el at i pointer
                 prod/=nums[i];
                 i++;
             }
+
             count+=(j-i+1);
             j++;
         }
