a707410236cf015ee6705222feabeed578a7dbab
Chandranshu <rishpandey59@gmail.com>
2025-06-01 11:32:02 +0530

    Time: 481 ms (27.99%), Space: 76.4 MB (87.77%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
@@ -7,31 +7,36 @@ class Solution {
                 return Double.compare(gainB,gainA); // max heap by gain
             }
         );
+        // a PQ which takes a array of size 2 as input(by default given size is two), where a[0] represents the pass students and a[1] represents totalt student of a class, same goes for b
+        // and we are calculating the gain by the two classes and sorting them on basis of max gain
 
         for(int[] cls: classes){
             pq.add(cls);
+        } // add all the elemts of classes intot the pq..
+
+        while(extraStudents>0){  // for every extra student
+            int highest[]=pq.remove();  // get the claass with net max gain
+            highest[0]++;  // update its pass count
+            highest[1]++; // update its total count.... like assigning
+            pq.add(highest);  // add it to the pq 
+            extraStudents--;  //reduce the count
         }
 
-        while(extraStudents>0){
-            int highest[]=pq.remove();
-            highest[0]++;
-            highest[1]++;
-            pq.add(highest);
-            extraStudents--;
-        }
-
-        double maxRatio=0.0;
+        double maxTotal=0.0;  // to get the sum of ratios of all which is a max ratiosum
         for(int[]cls: pq){
-            maxRatio+=(double) cls[0]/cls[1];
+            maxTotal+=(double) cls[0]/cls[1];
         }
 
-        return maxRatio/classes.length;
+        return maxTotal/classes.length;  // return the maxratio for whole system
     
         
     }
      public double gain(int pass, int total){  // a helper function to return the net gain by a class if we assign a new student to it
-        double old=(double)pass/total;
-        double neww =(double) (pass+1)/(total+1);
-        return neww-old;
+        double old=(double)pass/total; // old ratio
+        double neww =(double) (pass+1)/(total+1);  // new ratio after assigning a student 
+        return neww-old;  // return the net gain
      }
 }
+
+
+// the idea is...at every level assign the extra student to that class which will show the maximum gain in its ratio when it is assigned a single student
