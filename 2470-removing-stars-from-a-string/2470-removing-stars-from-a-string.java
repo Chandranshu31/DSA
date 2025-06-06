628dd51474189d91f6056bb723441aff9af04143
Chandranshu <rishpandey59@gmail.com>
2025-06-06 15:36:21 +0530

    Time: 33 ms (79.94%), Space: 45.7 MB (71.79%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.java
@@ -12,3 +12,31 @@ class Solution {
         return sb.toString();
     }
 }
+/** AN ALTER soln
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!='*'){
+                stack.push(s.charAt(i));
+            }
+            else{
+                stack.pop();
+            }
+        }
+        String temp="";
+        while(!stack.isEmpty()){
+            char ch=stack.pop();
+            temp+=ch;
+        }
+        
+        int j=temp.length()-1;
+        String res="";
+        while(j>=0){
+            res+=temp.charAt(j);
+            j--;
+        }
+        return res;
+        
+    }
+} */
