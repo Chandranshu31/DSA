ecc20bc52a7af6fd8892da83afe981758c941ae5
Chandranshu <rishpandey59@gmail.com>
2025-06-06 16:11:34 +0530

    Time: 38 ms (29.87%), Space: 45.4 MB (75.77%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -1,20 +1,29 @@
 class Solution {
     public String removeDuplicates(String s) {
      int n=s.length();
-     StringBuilder sb = new StringBuilder();
-     for(int i=0;i<n;i++){
-        if(sb.length()>0){
-            if(sb.charAt(sb.length()-1)==s.charAt(i)){  // if the prev element or last element added in stringulder is same as current character
-            // then its a duplicate case, so delete that element from sb and donta= add this one into the sb
-                sb.deleteCharAt(sb.length()-1);
-            }else{
-                sb.append(s.charAt(i)); // otherwise simply add
-            }
-        }else{
-            sb.append(s.charAt(i));  // if sblength<0 then simply add the char 
-        }
+     if(n==0)   return "";
+      
+     
+     Stack<Character> stack = new Stack<>();
+
+     stack.push(s.charAt(0));
+
+     for(int i=1;i<n;i++){
+        
+        if(!stack.isEmpty() && stack.peek()==s.charAt(i)) stack.pop();
+
+        else  stack.push(s.charAt(i));  
+
      }
+
+     StringBuilder sb = new StringBuilder();
+
+     while(!stack.isEmpty())  sb.append(stack.pop());
+       
+     sb.reverse();
      return sb.toString();
+
+
         
    
     }
