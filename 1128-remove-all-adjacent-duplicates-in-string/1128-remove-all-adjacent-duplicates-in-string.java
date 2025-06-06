4b52aa35c5371c3cdc0afff5f837d3fa0a48d15c
Chandranshu <rishpandey59@gmail.com>
2025-06-06 16:03:10 +0530

    Time: 15 ms (71.38%), Space: 45.3 MB (85.62%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -4,13 +4,14 @@ class Solution {
      StringBuilder sb = new StringBuilder();
      for(int i=0;i<n;i++){
         if(sb.length()>0){
-            if(sb.charAt(sb.length()-1)==s.charAt(i)){
+            if(sb.charAt(sb.length()-1)==s.charAt(i)){  // if the prev element or last element added in stringulder is same as current character
+            // then its a duplicate case, so delete that element from sb and donta= add this one into the sb
                 sb.deleteCharAt(sb.length()-1);
             }else{
-                sb.append(s.charAt(i));
+                sb.append(s.charAt(i)); // otherwise simply add
             }
         }else{
-            sb.append(s.charAt(i));
+            sb.append(s.charAt(i));  // if sblength<0 then simply add the char 
         }
      }
      return sb.toString();
@@ -18,3 +19,35 @@ class Solution {
    
     }
 }
+
+/**
+Another solution but with a repeatitive chekc over string s which is abit unnecessary 
+class Solution {
+    public String removeDuplicates(String s) {
+     int n=s.length();
+        
+    while (isRepeating(s)) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < n; i++) {
+        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == s.charAt(i)) {
+            sb.deleteCharAt(sb.length() - 1);
+        } else {
+            sb.append(s.charAt(i)); 
+        }
+      }
+      s = sb.toString(); // update the S to check if its still has repeting occurences 
+     }
+
+     return s;   
+    }
+    public boolean isRepeating(String s){
+        int n=s.length();
+        for(int i=1;i<n;i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+ */
