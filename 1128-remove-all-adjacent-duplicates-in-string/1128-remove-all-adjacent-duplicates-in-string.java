e222c73e5e3debd4dacbe02914e8c3e321bd85fd
Chandranshu <rishpandey59@gmail.com>
2025-06-06 16:00:35 +0530

    Time: 13 ms (75.49%), Space: 45.3 MB (85.62%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -1,28 +1,20 @@
 class Solution {
     public String removeDuplicates(String s) {
      int n=s.length();
-        
-    while (isRepeating(s)) {
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < n; i++) {
-        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == s.charAt(i)) {
-            sb.deleteCharAt(sb.length() - 1);
-        } else {
-            sb.append(s.charAt(i)); 
-        }
-      }
-      s = sb.toString(); // update the S to check if its still has repeting occurences 
-     }
-
-     return s;   
-    }
-    public boolean isRepeating(String s){
-        int n=s.length();
-        for(int i=1;i<n;i++){
-            if(s.charAt(i)==s.charAt(i-1)){
-                return true;
+     StringBuilder sb = new StringBuilder();
+     for(int i=0;i<n;i++){
+        if(sb.length()>0){
+            if(sb.charAt(sb.length()-1)==s.charAt(i)){
+                sb.deleteCharAt(sb.length()-1);
+            }else{
+                sb.append(s.charAt(i));
             }
+        }else{
+            sb.append(s.charAt(i));
         }
-        return false;
+     }
+     return sb.toString();
+        
+   
     }
 }
