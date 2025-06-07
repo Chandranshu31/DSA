f0ff4f0773e595c967289d15c0740e9a65e63232
Chandranshu <rishpandey59@gmail.com>
2025-06-07 11:16:41 +0530

    Time: 1 ms (99.5%), Space: 42.1 MB (78.83%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.java
+++ 1720-crawler-log-folder/1720-crawler-log-folder.java
@@ -1,15 +1,17 @@
 class Solution {
     public int minOperations(String[] logs) {
         int count = 0;
-        for (String log : logs) {
-            if (log.equals("../")) {
-                if (count > 0) count--;
-            } else if (log.equals("./")) {
+        for(String log: logs){
+            if(log.equals("../")){
+                if(count>0) count--; // because if already at the main foldeer then we cant go back to its parent as there is not any 
+            }
+            else if(log.equals("./")){  // stay in same folder
                 continue;
-            } else {
-                count++;
+            }
+            else{
+                count++;  // go to the next folder
             }
         }
-        return count;
+        return count; // how far u r from the main folder 
     }
 }
