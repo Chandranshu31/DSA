87bd2c962c11ec628a73e5198087492bce2c0e9e
Chandranshu <rishpandey59@gmail.com>
2025-06-10 18:15:05 +0530

    Time: 1 ms (100%), Space: 46.7 MB (39.5%) - LeetHub

--- 1765-merge-in-between-linked-lists/1765-merge-in-between-linked-lists.java
+++ 1765-merge-in-between-linked-lists/1765-merge-in-between-linked-lists.java
@@ -10,26 +10,31 @@
  */
 class Solution {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        // given that a and b wil always be between 1 and length of l1-1 so tail node wont be there in b
+        // set a pointer at the tail of list2
         
-        ListNode tailOfList2=list2;
+        ListNode tailOfList2=list2; 
         while(tailOfList2.next!=null){
             tailOfList2=tailOfList2.next;
         }
-        int count=1;
+
+        int count=1;      // after that move the pointer until we reach behind the ath  indexed node
         ListNode temp=list1;
         while(count<a){
             temp=temp.next;
             count++;
         }
-        ListNode forward=temp.next;
+
+        ListNode forward=temp.next;  // store the next and make temp pont to th list1 head
         temp.next=list2;
-        while(count<b){
+        while(count<b){    // same for b iterate until we reach  the b
             forward=forward.next;
             count++;
         }
-        ListNode end=forward.next;
-        forward.next=null;
-        tailOfList2.next=end;
+
+        ListNode end=forward.next;  // just ahead or next of bth node
+        forward.next=null;  //break the link from original
+        tailOfList2.next=end;  // make the tail point to it
 
         return list1;
 
