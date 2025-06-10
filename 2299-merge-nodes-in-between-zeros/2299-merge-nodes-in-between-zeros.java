33a34c80f3c809df19e880c49eb2b9e1ba477af8
Chandranshu <rishpandey59@gmail.com>
2025-06-10 17:51:13 +0530

    Time: 5 ms (82.51%), Space: 84 MB (10.53%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.java
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.java
@@ -13,24 +13,24 @@ class Solution {
         if(head==null){
             return null;
         }
+        
         ListNode dummy = new ListNode(-1);
-        ListNode aux=dummy;
-        ListNode temp=head.next;
-        while(temp!=null){
-            int sum=0;
-            while(temp.val!=0){
+        ListNode aux=dummy; // pointer over the dummy node
+        ListNode temp=head.next;  // start from the next of head ashead is always==0
+        while(temp!=null){  // iterate over the given linkedlist
+            int sum=0;     // to store the running sum
+            while(temp.val!=0){  // until we get a 0 keep moving the temp and adding the values inot sum
                 sum+=temp.val;
                 temp=temp.next;
             }
-            ListNode newNode = new ListNode(sum);
-            aux.next=newNode;
-            aux=aux.next;
-
-             temp=temp.next;
-            sum=0;
+            ListNode newNode = new ListNode(sum);  // now create a node with value of sum
+            aux.next=newNode;  // make aux point towards it
+            aux=aux.next;  // move aux ahead to add next node if its there
+            temp=temp.next;   // move temp ahead
+            sum=0;  // set sum==0 again. for the next iteration
             
         }
-        return dummy.next;
+        return dummy.next; // return the head of the new linked list
         
     }
 }
