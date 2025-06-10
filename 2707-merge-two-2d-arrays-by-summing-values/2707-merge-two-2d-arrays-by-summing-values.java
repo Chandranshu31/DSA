4d68857c5235854339c4e9473ad847ab33855d85
Chandranshu <rishpandey59@gmail.com>
2025-06-10 23:33:48 +0530

    Time: 4 ms (29.12%), Space: 45.3 MB (66.9%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -22,3 +22,50 @@ class Solution {
         return result;
     }
 }
+
+/**
+class Solution {
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums1.length;i++){
+            map.put(nums1[i][0],nums1[i][1]);
+        }
+
+        List<List<Integer>> temp = new ArrayList<>();
+        for(int i=0;i<nums2.length;i++){
+            int currId=nums2[i][0];
+            List<Integer> aux = new ArrayList<>();
+            if(map.containsKey(currId)){
+                   aux.add(currId);
+                   aux.add(nums2[i][1]+map.get(currId));
+                   map.put(currId,-1);
+            }else{
+                aux.add(currId);
+                aux.add(nums2[i][1]);
+            }
+            temp.add(aux);
+        }
+
+        for(int k:map.keySet()){   // if there are any elements or ids that are in n1 andnot in n2 then after above itration their values wont be -1 so just add them inot the list as well
+            if(map.get(k)!=-1){
+             List<Integer> aux= new ArrayList<>();
+             aux.add(k);
+             aux.add(map.get(k));
+             temp.add(aux);
+            }
+        }
+
+        int p=temp.size();
+        int res[][]=new int[p][2];
+
+        for(int i=0;i<p;i++){
+            res[i][0]=temp.get(i).get(0);
+            res[i][1]=temp.get(i).get(1);
+        }
+        Arrays.sort(res, (a, b)->Integer.compare(a[0], b[0])); // sort on the basis of id.
+
+        return res; 
+        
+        
+    }
+} */
