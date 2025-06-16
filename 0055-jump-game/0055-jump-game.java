a5e7ab2238e42e2c0c340ddbfee1d396bf45a451
Chandranshu <rishpandey59@gmail.com>
2025-06-16 11:12:15 +0530

    Time: 3 ms (50.65%), Space: 45 MB (99.5%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean canJump(int[] nums) {
-       int farthest=0;
+       int farthest=0; // how far could i go till now 
        for(int i=0;i<nums.length;i++){
-        if(i>farthest) return false;
+        if(i>farthest) return false;   // am I standing at an index (i) that is beyond how far I could jump?
         farthest=Math.max(farthest,i+nums[i]);
        }
        return true;
