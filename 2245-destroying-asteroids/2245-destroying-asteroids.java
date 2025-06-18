b13b03cad6d303c250af21cb4dcbdb624a274da3
Chandranshu <rishpandey59@gmail.com>
2025-06-18 11:08:15 +0530

    Time: 20 ms (95.99%), Space: 59.8 MB (86.72%) - LeetHub

--- 2245-destroying-asteroids/2245-destroying-asteroids.java
+++ 2245-destroying-asteroids/2245-destroying-asteroids.java
@@ -2,10 +2,10 @@ class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
         Arrays.sort(asteroids);
         long currMass=mass;
-       // boolean canBeDestroyed=true;
-        for(int i=0;i<asteroids.length;i++){
-            if(currMass>=asteroids[i]){
-                currMass+=asteroids[i];
+     
+        for(int asteroid: asteroids){
+            if(currMass>=asteroid){
+                currMass+=asteroid;
             }else{
                 return false;
             }
