bb2dc2240b4e3a888f06e361a32277b712dbe9d2
Chandranshu <rishpandey59@gmail.com>
2025-06-20 16:07:10 +0530

    Time: 0 ms (100%), Space: 41 MB (97.35%) - LeetHub

--- 1848-sum-of-unique-elements/1848-sum-of-unique-elements.java
+++ 1848-sum-of-unique-elements/1848-sum-of-unique-elements.java
@@ -1,6 +1,6 @@
 class Solution {
     public int sumOfUnique(int[] nums) {
-        int map[]=new int[101];
+        int map[]=new int[101];  // given the isze of 100
         for(int i=0;i<nums.length;i++){
             map[nums[i]]++;
         }
@@ -12,8 +12,24 @@ class Solution {
             }
         }
         return sum;
+    }
+}
+/**
+can be solved using a map also 
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int num: nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        int sum=0;
+      for(int key: map.keySet()){  // can also use for(Mpa.Entry<Integer,Integer> entry: map.entrySet()){...}
+        if(map.get(key)==1){
+            sum+=key;
+        }
+      }
 
-     
+        return sum;
     }
     
-}
+} */
