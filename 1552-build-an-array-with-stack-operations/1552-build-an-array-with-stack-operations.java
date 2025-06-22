f7ed01a47acafccd769ffc6b0e10fd97e1d06b40
Chandranshu <rishpandey59@gmail.com>
2025-06-22 15:27:24 +0530

    Time: 0 ms (100%), Space: 42.8 MB (48.75%) - LeetHub

--- 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.java
+++ 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.java
@@ -21,3 +21,30 @@ class Solution {
        return res; 
     }
 }
+
+/**
+can also be solved uisnng a set
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+       int k=target.length;
+       HashSet<Integer> set = new HashSet<>();
+       for(int num:target){
+        set.add(num);
+       }
+       int matched=0;
+       List<String> res = new ArrayList<>();
+       for(int i=1;i<=n;i++){
+        if(set.contains(i)){
+            res.add("Push");
+            matched++;
+            if(matched==k) break;
+        }else{
+            res.add("Push");
+            res.add("Pop");
+        }
+       }
+
+       return res; 
+    }
+}
+ */
