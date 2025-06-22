1e658a011e0c4ff59391afa96f10b35d7f94fa88
Chandranshu <rishpandey59@gmail.com>
2025-06-22 15:26:42 +0530

    Time: 0 ms (100%), Space: 42.9 MB (48.75%) - LeetHub

--- 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.java
+++ 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.java
@@ -1,21 +1,21 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
        int k=target.length;
-       HashSet<Integer> set = new HashSet<>();
-       for(int num:target){
-        set.add(num);
-       }
-       int matched=0;
+      
+       int idx=0;
+
        List<String> res = new ArrayList<>();
        for(int i=1;i<=n;i++){
-        if(set.contains(i)){
+        if(idx==k)  break;  // the stacked matched with target so no need to consider further stream elements 
+
+        if(i==target[idx]){
             res.add("Push");
-            matched++;
-            if(matched==k) break;
+            idx++;
         }else{
             res.add("Push");
             res.add("Pop");
         }
+     
        }
 
        return res; 
