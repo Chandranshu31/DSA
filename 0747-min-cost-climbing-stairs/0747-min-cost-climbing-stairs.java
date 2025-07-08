0207022e250215aab8c3620987c7b9c63f90bb69
Chandranshu <rishpandey59@gmail.com>
2025-07-08 23:28:28 +0530

    Time: 0 ms (100%), Space: 42.9 MB (95.43%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,16 +1,20 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-    int n = cost.length;
-    int[] dp = new int[n];
+        int n=cost.length; // the top is n only
+        // we can either take a 1 step or 2 step at a time. so to reach the ith pos we can either come from
+        // i-1 or i-2, and we need to find the min cost to reach i so that would minimum of cost to reach i-1 and i-2.
 
-    dp[0] = cost[0];
-    dp[1] = cost[1];
-
-    for (int i = 2; i < n; i++) {
-        dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
-    }
-
-    return Math.min(dp[n - 1], dp[n - 2]); 
+        int dp[]=new int[n]; // kind of cost to reach ith index array
+        // we can either start from index 0 or 1 so to reach them cost would be ewual to their value only
         
-    }
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+
+        for(int i=2;i<dp.length;i++){
+            dp[i]=Math.min(dp[i-1],dp[i-2])+cost[i];  // min cost to reach ith idx
+        }
+
+        return Math.min(dp[n-1],dp[n-2]);  // we can reach ith from i-1 or i-2 then min would be the minnimum of cost req to reach i-1 or i-2
+        
+    }  // this is tabulation method
 }
