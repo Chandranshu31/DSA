1baa08e5747704b311b2d741a3d1514454abfc37
Chandranshu <rishpandey59@gmail.com>
2025-07-08 22:56:37 +0530

    Time: 0 ms (100%), Space: 40.3 MB (64.1%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,5 +1,25 @@
 class Solution {
     public int fib(int n) {
+        int dp[]=new int[n+1];
+        return calc(n,dp);
+    }
+    public int calc(int n, int dp[]){
+        if(n==0||n==1){
+            return n;
+        }
+        if(dp[n]!=0){
+            return dp[n];
+        }
+
+        dp[n]=calc(n-1,dp)+calc(n-2,dp);
+        return dp[n];
+    }
+} // this memoization (top to bottom)
+
+/**
+This is tabulation where we filled the dp table
+class Solution {
+    public int fib(int n) {
         if(n==0||n==1){
             return n;
         }
@@ -13,3 +33,6 @@ class Solution {
         
     }
 }
+
+
+} */
