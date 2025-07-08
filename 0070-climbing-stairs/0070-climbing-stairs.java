28f3e6d44344430bbf17e7525adeded09b00bd80
Chandranshu <rishpandey59@gmail.com>
2025-07-08 23:07:56 +0530

    Time: 0 ms (100%), Space: 40.1 MB (96.77%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,21 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[]= new int[n+1];
-        return calc(n,dp);
-    }
-    public int calc(int n, int dp[]){
+        // to reach a stair number n we can either jump form n-1 or n-2 so to get the number of ways to reach 
+        // nth stair, if we calculate the number of ways to reach n-1 and n-2 and simply sum them, that would be the number
+        // of ways to climb the nth stair, as we can take a jump of 1 or 2 only so reach n, we would either take the jump from
+        // n-1 or n-2;
+
         if(n==0||n==1){
             return 1;
         }
-        if(dp[n]!=0){
-            return dp[n];
+        
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<dp.length;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        dp[n]=calc(n-1,dp)+calc(n-2,dp);
         return dp[n];
+        
     }
 }
