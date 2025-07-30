83a4b2d3b87e5eb93c71efe71ec5d1aaa266035c
Chandranshu <rishpandey59@gmail.com>
2025-07-30 21:53:10 +0530

    Time: 0 ms (100%), Space: 40.7 MB (17.11%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -1,12 +1,37 @@
 class Solution {
     public int tribonacci(int n) {
-        if(n==0){
+     int dp[]= new int[n+1];
+     return calc(n , dp);
+        
+    }
+    public int calc(int n, int dp[]){
+     if(n==0 || n==1){
+        return n;
+     }
+     if(n==2){
+        return 1;
+     }
+     if(dp[n]!=0){
+        return dp[n];
+     }
+     dp[n]=calc(n-1,dp)+calc(n-2,dp)+calc(n-3,dp);
+     return dp[n];
+    }
+}
+/**
+Tabulation class Solution {
+    public int tribonacci(int n) {
+        int dp[]=new int[n+1];
+        return calc(n,dp);
+        
+    }
+    public int calc(int n, int dp[]){
+        if(n==0||n==1){
             return n;
         }
-        if(n==1||n==2){
+        if(n==2){
             return 1;
         }
-        int dp[]=new int[n+1];
         dp[0]=0;
         dp[1]=1;
         dp[2]=1;
@@ -15,4 +40,4 @@ class Solution {
         }
         return dp[n];
     }
-}
+} */
