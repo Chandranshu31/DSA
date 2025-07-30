a8b68aaca252b4f832e4d25b866f40b47d2c0a30
Chandranshu <rishpandey59@gmail.com>
2025-07-30 21:38:27 +0530

    Time: 0 ms (100%), Space: 40.7 MB (27.08%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,38 +1,18 @@
 class Solution {
     public int fib(int n) {
-        int dp[]=new int[n+1];
-        return calc(n,dp);
+        int dp[]= new int[n+1]; // where 0th indx of this array stores the 0th fib number
+         return calc(dp,n);
+        
     }
-    public int calc(int n, int dp[]){
-        if(n==0||n==1){
+    public int calc(int dp[],int n){
+        if(n==0 || n==1){
             return n;
         }
-        if(dp[n]!=0){
-            return dp[n];
-        }
-
-        dp[n]=calc(n-1,dp)+calc(n-2,dp);
-        return dp[n];
-    }
-} // this memoization (top to bottom)
-
-/**
-This is tabulation where we filled the dp table
-class Solution {
-    public int fib(int n) {
-        if(n==0||n==1){
-            return n;
-        }
-        int dp[]=new int[n+1];
         dp[0]=0;
         dp[1]=1;
         for(int i=2;i<dp.length;i++){
             dp[i]=dp[i-1]+dp[i-2];
-        }
+        } 
         return dp[n];
-        
     }
 }
-
-
-} */
