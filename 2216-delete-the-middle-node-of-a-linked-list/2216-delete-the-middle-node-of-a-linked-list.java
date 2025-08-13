986759d8c1226c0feab7a31557c1f2833c6d8115
Chandranshu <rishpandey59@gmail.com>
2025-08-13 18:19:57 +0530

    Time: 3 ms (99.74%), Space: 64.2 MB (42.55%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.java
@@ -10,27 +10,30 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        if(head==null){
+        if(head==null||head.next==null){
             return null;
         }
-        if(head.next==null){
-            return null;
-        }
-        ListNode dummy = new ListNode(-2);
+       
+
+        ListNode dummy=new ListNode(-1);
+
         dummy.next=head;
 
         ListNode fast=head;
-        ListNode slow= head;
-        ListNode prev= dummy;
-        
+        ListNode slow=head;
+        ListNode prev=dummy;
 
-        while(fast!=null && fast.next!=null){
+        while(fast!=null&&fast.next!=null){
             fast=fast.next.next;
             slow=slow.next;
             prev=prev.next;
         }
-        prev.next=prev.next.next;
+        prev.next=slow.next;
+
         return head;
+
         
     }
 }
+/** could be solved without using dummy node by initialsing fast from head.next.next and slow from head and then
+moving them according to their pace, at the end slow would be 1 node behind of middle. */
