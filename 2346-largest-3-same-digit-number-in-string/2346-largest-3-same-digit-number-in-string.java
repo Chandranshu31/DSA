3333df5569ef8538257328706e936119b59c1e01
Chandranshu <rishpandey59@gmail.com>
2025-08-14 21:31:30 +0530

    Time: 2 ms (81.68%), Space: 41.8 MB (92.25%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
@@ -1,19 +1,19 @@
 class Solution {
     public String largestGoodInteger(String num) {
         StringBuilder sb = new StringBuilder();
-        int largest=-1;
+        int largest=Integer.MIN_VALUE;
         for(int i=0;i<=num.length()-3;i++){
             if(num.charAt(i)-'0'==num.charAt(i+1)-'0' && num.charAt(i)-'0'==num.charAt(i+2)-'0'){
                 largest=Math.max(largest,num.charAt(i)-'0');
-                i+=2;
+                i+=2; // if triplet found then simply move two steps ahead as no neeed to consider the next two indices as they are same as prev one.
             }
         }
-        if(largest==-1){
+        if(largest==-Integer.MIN_VALUE){
             return "";
         }
 
         for(int i=0;i<3;i++){
-          sb.append((char) (largest + '0'));
+          sb.append(largest);
         }
 
         return sb.toString();
