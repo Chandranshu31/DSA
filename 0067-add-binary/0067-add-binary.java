a30cfbda011bb73b47af88acd0e69005cbce50a4
Chandranshu <rishpandey59@gmail.com>
2025-08-17 18:05:03 +0530

    Time: 2 ms (41.52%), Space: 42.4 MB (51.95%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -14,11 +14,11 @@ class Solution {
                 sum+=b.charAt(j)-'0';
                 j--;
             }
-            sb.append(sum%2);  // if sum==0 then 0+0 means just add 0%2, if 1 then (0+1 or vice versa) then add 1%2 that is 1.. in case of 1+1 add 2%2 that is 0 and in this case only ca
+            sb.insert(0,sum%2);  // if sum==0 then 0+0 means just add 0%2, if 1 then (0+1 or vice versa) then add 1%2 that is 1.. in case of 1+1 add 2%2 that is 0 and in this case only ca
             // carry would be 1.
             carry=sum/2;  // 0/2=0, 1/2=0, 2/2=1 only in 1+1 case carry would become 1
         }
-        sb.reverse(); // in builder we r storng from front but adding from back, so just reverse the result.
+       // sb.reverse(); // in builder we r storng from front but adding from back, so just reverse the result.
         return sb.toString();
         
     }
