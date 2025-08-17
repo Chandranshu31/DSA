c1ad17b02e2a60db6f379488e0cf2e084a53f391
Chandranshu <rishpandey59@gmail.com>
2025-08-17 15:47:07 +0530

    Time: 2 ms (56.84%), Space: 45.1 MB (73.71%) - LeetHub

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.java
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.java
@@ -26,12 +26,8 @@ class Solution {
         return head;
         
     }
-  public int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = a % b;
-            a = b;
-            b = temp;
-        }
-        return a;
-    }
+  private int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
 }
