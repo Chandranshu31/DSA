a69c35f0b1d959956919ce76819497b2537e5389
Chandranshu <rishpandey59@gmail.com>
2025-08-19 18:42:13 +0530

    Time: 23 ms (98.03%), Space: 49.7 MB (87.53%) - LeetHub

--- 0912-random-pick-with-weight/0912-random-pick-with-weight.java
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.java
@@ -33,6 +33,11 @@ class Solution {
     }
 }
 
+// we are suppossed to randomly return a weight but the wight with a greater value has more probability of getting returned
+// so we calculated prefix sum that generated a range for them... like for[1,3,6]  prefix woudl be [1,4,10] so indx 0 rep 1...indx 1 has 3 bucket values 2,3,4.
+// and then randomly generate a nummber in rnge of 1 and ttoal... and then use bin search to fidn the first numb that is bigger or equal to it.. that would return the indx of that weight also
+// this is how we managed to biased the prpbability of getting a higher weight indx more.
+
 /**
  * Your Solution object will be instantiated and called as such:
  * Solution obj = new Solution(w);
