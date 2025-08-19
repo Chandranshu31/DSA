0f927daa0b916cf17420c4a5eb6e4be5e1dca997
Chandranshu <rishpandey59@gmail.com>
2025-08-19 23:43:32 +0530

    Time: 34 ms (35.73%), Space: 55.5 MB (87.26%) - LeetHub

--- 0931-maximum-frequency-stack/0931-maximum-frequency-stack.java
+++ 0931-maximum-frequency-stack/0931-maximum-frequency-stack.java
@@ -1,6 +1,6 @@
 class FreqStack {
-    HashMap<Integer,Integer> freq;
-    HashMap<Integer, Stack<Integer>> group;
+    HashMap<Integer,Integer> freq;  // stores the frequency of a number
+    HashMap<Integer, Stack<Integer>> group;  // key is the freq and value is all the numbes having those freq in a stack.... so that if fre colide we cna get the most reentone by top of stack, LIFO
     int maxFreq;
 
     public FreqStack() {
@@ -11,21 +11,21 @@ class FreqStack {
     }
     
     public void push(int val) {
-        int f=freq.getOrDefault(val,0)+1; 
-        freq.put(val,f);
+      int f=freq.getOrDefault(val,0)+1; // get the freq of val, if dnethen 0+1 or if exists then +1
+        freq.put(val,f); // put it into the map now with new freq
 
-        maxFreq=Math.max(f,maxFreq);
+        maxFreq=Math.max(f,maxFreq);  // update max feq if possible
 
-        if (!group.containsKey(f)) {
+        if (!group.containsKey(f)) {  // if group dne then create a key of this freq and a stack correspondindg to it
          group.put(f, new Stack<>());
          }
-         group.get(f).push(val);   
+         group.get(f).push(val);     // then push the val inot that stack
     }
     
     public int pop() {
-        int x=group.get(maxFreq).pop();
-        freq.put(x,freq.get(x)-1);
-         if (group.get(maxFreq).isEmpty()) {
+        int x=group.get(maxFreq).pop();  // get the most reent numb with maxfreq by map->correspondng key of maxfreq then value of stack then pop for most recent one
+        freq.put(x,freq.get(x)-1);  // update the frequency of current numb
+         if (group.get(maxFreq).isEmpty()) {  // if group is empty now for the key then reduce amxfreq as thee are no numbers there
             maxFreq--;
         }
         return x;
