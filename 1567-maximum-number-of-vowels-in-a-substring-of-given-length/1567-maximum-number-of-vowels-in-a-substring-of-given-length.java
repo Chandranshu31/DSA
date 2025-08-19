b5fa2c33fe75d4c4156650a221f8a52accdcdb46
Chandranshu <rishpandey59@gmail.com>
2025-08-19 09:55:15 +0530

    Time: 13 ms (75.49%), Space: 44.9 MB (42.1%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,31 +1,29 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        int maxNumb=0;
-        int n=s.length();
-        for(int i=0;i<k;i++){   // check  the number of vowels in the first window, and make it the curr maxnumb
+        if(s.length()==0 || s.length()<k) return 0;
+        if(s.length()==1) return 1;
+        int count=0;
+        int maxCount=Integer.MIN_VALUE;
+        for(int i=0;i<k;i++){
             if(isVowel(s.charAt(i))){
-                maxNumb++;
-            }
-        }
-        int count=maxNumb; // asssign the curr count as maxnumb
-        for(int i=k;i<n;i++){
-            if(isVowel(s.charAt(i-k))){  // is the char going out of this window ws vowel decrease the count , currcount
-                count--;
-            }
-            if(isVowel(s.charAt(i))){  // if the element coming in window is vowel increment the count
                 count++;
             }
-            maxNumb=Math.max(count,maxNumb);  // keep updating the maxnumb after every iteration
-
+            if(count>1) maxCount=Math.max(count,maxCount);
         }
+        for(int i=k;i<s.length();i++){
+            if(isVowel(s.charAt(i-k))){
+                count--;
+            }
+            if(isVowel(s.charAt(i))){
+                count++;
+            }
+            maxCount=Math.max(count,maxCount);
+        }
+        return maxCount;
 
-        return maxNumb;
+        
     }
-
-    public boolean isVowel(char ch){  // fucn to check if a char is vowel or not
-        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
-            return true;
-        }
-        return false;
+    public boolean isVowel(char c){
+        return c=='a'||c=='e' || c=='i' || c=='o' || c=='u';
     }
 }
