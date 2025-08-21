2f41155b55997ba6a9045da1d62d308a5cedc071
Chandranshu <rishpandey59@gmail.com>
2025-08-21 23:40:54 +0530

    Time: 5 ms (99.59%), Space: 45.2 MB (34.69%) - LeetHub

--- 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.java
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.java
@@ -1,20 +1,29 @@
 class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
         Arrays.sort(nums);
-        int res[] = new int[queries.length];
+        int prefix[]=new int[nums.length];
+        prefix[0]=nums[0];
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+nums[i];
+        }
+        int res[]=new int[queries.length];
         for(int i=0;i<queries.length;i++){
-            int curr=queries[i];
-            int sum=0;
-            int length=0;
-            for(int j=0;j<nums.length;j++){
-                sum+=nums[j];
-                if(sum<=curr){
-                    length++;
-                }
-            }
-            res[i]=length;
+            res[i]=firstGreaterIdx(prefix,queries[i]);
         }
         return res;
-        
+    }
+    public int firstGreaterIdx(int arr[], int x){
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]<=x){
+                start=mid+1;
+            }else{
+                end=mid;
+            }
+        }
+        if (arr[start] <= x) return arr.length; // atp start would be at the last index
+    return start;
     }
 }
