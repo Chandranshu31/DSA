d9b6e0e3278d2d6ff99b15326b508066517d6ed6
Chandranshu <rishpandey59@gmail.com>
2025-08-22 10:25:26 +0530

    Time: 5 ms (94.31%), Space: 197.3 MB (14.63%) - LeetHub

---
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minimumArea(int[][] grid) {
+        int top=-1,bottom=-1,left=-1,right=-1;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+               if(grid[i][j]==1){
+                 if (top == -1) {  // first 1 found
+                        top = bottom = i;
+                        left = right = j;
+                    } else {
+                        top = Math.min(top, i);
+                        bottom = Math.max(bottom, i);
+                        left = Math.min(left, j);
+                        right = Math.max(right, j);
+                    }
+               }
+            }
+        }
+        return (bottom-top+1)*(right-left+1);
+    }
+}
+
+/**
+top → the smallest row index containing a 1
+
+bottom → the largest row index containing a 1
+
+left → the smallest column index containing a 1
+
+right → the largest column index containing a 1
+so bottom-top+1 would give us the height and right-left+1 would give us the length */
