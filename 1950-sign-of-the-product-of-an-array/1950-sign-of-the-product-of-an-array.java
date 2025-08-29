dbe502f279d89780d92867f5f8e0b990e739200b
Chandranshu <rishpandey59@gmail.com>
2025-08-30 00:36:29 +0530

    Time: 0 ms (100%), Space: 44.5 MB (11.16%) - LeetHub

--- 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
@@ -1,13 +1,14 @@
 class Solution {
     public int arraySign(int[] nums) {
-        if(nums.length==1){
+        if(nums.length==1){  // edge case when only one element
             int a=nums[0];
             if(a==0) return 0;
             return a>0?1:-1;
         }
-        int neg=0;
+
+        int neg=0; // count the number of negative ones... if odd tehen prod is obv neg and for even its pos so 1
         for(int n:nums){
-            if(n==0) return 0;
+            if(n==0) return 0; // if any elemnt is zero then prod is zero also
             if(n<0) neg++;
         }
         if(neg==0){
