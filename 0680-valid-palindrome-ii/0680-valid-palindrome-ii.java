380a0fdb05650150fd58c7cc972cc73c9aa12366
Chandranshu <rishpandey59@gmail.com>
2025-09-06 00:04:35 +0530

    Time: 4 ms (99.62%), Space: 45.6 MB (41.86%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }else{ // in case of a mismatch, try skipping either of the characters once and by skipping any one of them we get true, then we can say that original string is a palindrome if we a del a single character
+                return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
+            }
+        }
+        return true;
+
+        
+    }
+    public boolean isPalindrome(String s, int x, int y){
+        int i=x;
+        int j=y;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
