5ea46dd99abf042e705e58b411885c71d260ed6c
Chandranshu <rishpandey59@gmail.com>
2025-09-19 01:10:50 +0530

    Time: 127 ms (90.46%), Space: 189.7 MB (94.83%) - LeetHub

--- 3707-find-the-most-common-response/3707-find-the-most-common-response.java
+++ 3707-find-the-most-common-response/3707-find-the-most-common-response.java
@@ -1,7 +1,8 @@
 class Solution {
     public String findCommonResponse(List<List<String>> responses) {
-        TreeMap<String,Integer> map = new TreeMap<>();
+        HashMap<String,Integer> map = new HashMap<>();
         int maxFreq=0;
+        String res="" ; // to store the best video response
         for(List<String> r : responses){
             HashSet<String> seen = new HashSet<>();
             for(int i=0;i<r.size();i++){
@@ -9,16 +10,16 @@ class Solution {
                 if(!seen.contains(curr)){
                       map.put(curr,map.getOrDefault(curr,0)+1);
                       seen.add(curr);
-                      maxFreq=Math.max(maxFreq,map.get(curr));
+                     int freq=map.get(curr);
+                     if(freq>maxFreq || (freq==maxFreq && curr.compareTo(res)<0)){
+                        res=curr;
+                        maxFreq=freq;
+                     }
                 }
             }
         }
-        for(String r: map.keySet()){
-            if(map.get(r)==maxFreq){
-                return r;
-            }
-        }
-        return " ";
+       
+        return res;
         
     }
 }
