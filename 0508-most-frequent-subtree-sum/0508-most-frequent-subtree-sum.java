ceae588accdb70df9e4a95431772fe24a147ce93
Chandranshu <rishpandey59@gmail.com>
2025-09-21 01:26:27 +0530

    Time: 4 ms (83.75%), Space: 45.6 MB (38.5%) - LeetHub

--- 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.java
+++ 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.java
@@ -14,20 +14,20 @@
  * }
  */
 class Solution {
-    HashMap<Integer,Integer> map = new HashMap<>();
-    int maxFreq=0;
+    HashMap<Integer,Integer> map = new HashMap<>();// to store sum and its freq
+    int maxFreq=0; // we will update it thoroughly
     public int[] findFrequentTreeSum(TreeNode root) {
         if(root==null) return new int[0];
 
-        getSubTreeSum(root);
+        getSubTreeSum(root); // its return values would be discarded as they r of no need
         ArrayList<Integer> temp = new ArrayList<>();
-        for(int k: map.keySet()){
+        for(int k: map.keySet()){ // add all maxfreq keys or sums inot list
             if(map.get(k)==maxFreq){
              temp.add(k);
             }
         }
 
-        int res[]=new int[temp.size()];
+        int res[]=new int[temp.size()]; // convert it into the array
         for(int i=0;i<res.length;i++){
             res[i]=temp.get(i);
         }
@@ -38,13 +38,13 @@ class Solution {
     public int getSubTreeSum(TreeNode root){
         if(root==null) return 0;
 
-        int left=getSubTreeSum(root.left);
-        int right=getSubTreeSum(root.right);
+        int left=getSubTreeSum(root.left); // leftsubtrree sum
+        int right=getSubTreeSum(root.right); // right subtree sum
+  
+        int currSum=left+right+root.val; // currsum including root
+        map.put(currSum,map.getOrDefault(currSum,0)+1);  // update in map
+        maxFreq=Math.max(maxFreq,map.get(currSum)); // update maxFreq
 
-        int currSum=left+right+root.val;
-        map.put(currSum,map.getOrDefault(currSum,0)+1);
-        maxFreq=Math.max(maxFreq,map.get(currSum));
-
-        return currSum;
+        return currSum; // return currsum to the upper level
     }
 }
