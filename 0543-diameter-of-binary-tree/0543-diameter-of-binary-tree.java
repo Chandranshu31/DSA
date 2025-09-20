dbd782485167eac17222256f9fd99f641b63eb91
Chandranshu <rishpandey59@gmail.com>
2025-09-21 00:59:48 +0530

    Time: 0 ms (100%), Space: 44.8 MB (51.86%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -16,7 +16,7 @@
 class Solution {
     public int maxDiam=0;
     public int diameterOfBinaryTree(TreeNode root) {
-        height(root);
+        height(root); // this is valid in java, as it does not throws any kind of error, the return value is discarde all the time
         return maxDiam;
     }
     public int height(TreeNode root){
@@ -24,7 +24,20 @@ class Solution {
         int lh=height(root.left);
         int rh=height(root.right);
 
-        maxDiam=Math.max(lh+rh,maxDiam);
+        maxDiam=Math.max(lh+rh,maxDiam); // when asked diameter wrt numb of nodes just math.max(lh+rh+1,maxdiam)
         return Math.max(lh,rh)+1;
     }
 }
+/*
+ * Height(TreeNode node) recursively computes the height of the subtree rooted at 'node'.
+ * While computing the height, we also update 'maxDiameter' as a side effect.
+ * 
+ * maxDiameter stores the largest number of edges on any path between two nodes found so far.
+ * For each node:
+ *   - leftHeight  = height of left subtree
+ *   - rightHeight = height of right subtree
+ *   - leftHeight + rightHeight gives the number of edges in the longest path through this node
+ *   - We update maxDiameter if this path is longer than the previous maximum.
+ * 
+ * Finally, getHeight returns the height to the parent so that the parent can compute its own potential diameter.
+ */
