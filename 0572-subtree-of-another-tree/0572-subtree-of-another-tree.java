810b1a61a9127f6bacfee37ca9d17037fce84c01
Chandranshu <rishpandey59@gmail.com>
2025-10-02 11:37:43 +0530

    Time: 2 ms (98.11%), Space: 44.4 MB (53.84%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -15,33 +15,27 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-
-        if(root==null){
-            return false;
-        }
-
-        if(root.val==subRoot.val){
+        if(root==null) return false;
+        if(root.val==subRoot.val) {
             if(isIdentical(root,subRoot)){
-                return true;
+                   return true;
             }
         }
 
         return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
         
     }
-
-    public static boolean isIdentical(TreeNode node,TreeNode subRoot){  // here node is the node where root.val==subRoot.val
-      if(node==null&&subRoot==null){
+    public boolean isIdentical(TreeNode root, TreeNode subRoot){
+        if(root==null && subRoot==null) return true;
+        if(root==null || subRoot==null || root.val!=subRoot.val){
+            return false;
+        }
+        if(!isIdentical(root.left,subRoot.left)){
+            return false;
+        }
+        if(!isIdentical(root.right,subRoot.right)){
+            return false;
+        }
         return true;
-      }else if(node==null|| subRoot==null || node.val!=subRoot.val){
-        return false;
-      }
-      if(!isIdentical(node.left,subRoot.left)){
-        return false;
-      }
-      if(!isIdentical(node.right,subRoot.right)){
-        return false;
-      }
-      return true;
     }
 }
