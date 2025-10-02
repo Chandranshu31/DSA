d215c18196f43d18b86fe0231e5a4fc8d3610758
Chandranshu <rishpandey59@gmail.com>
2025-10-03 00:16:54 +0530

    Time: 1 ms (99.89%), Space: 45 MB (18.93%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -16,30 +16,26 @@
  // A BRUTE FORCE APPROACH WHERE WE GENERATE ALL THE ROOT TO LEAF PATH AND THEN CHECK WETHER THE VALUES ADD UPTO THE TARGET SUM OR NOT
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> list = new ArrayList<>();
-        dfs(root,new ArrayList<>(),list);
         List<List<Integer>> res = new ArrayList<>();
-        for(List<Integer> path: list){
-            int sum=0;
-            for(int num:path){
-                sum+=num;
-            }
-            if(sum==targetSum){
-                res.add(new ArrayList<>(path));
-            }
-        }
+        dfs(root,0,new ArrayList<>(),res,targetSum);
         return res;
+        
     }
-    public void dfs(TreeNode root, List<Integer> path, List<List<Integer>> list){
+    public void dfs(TreeNode root,int sum, List<Integer> path, List<List<Integer>> list,int targetSum){
         if(root==null) return;
         path.add(root.val);
+        sum+=root.val;
         if(root.left==null && root.right==null){
-            list.add(new ArrayList<>(path));
+            if(sum==targetSum){
+                list.add(new ArrayList<>(path));
+            }
         }else{
-            dfs(root.left,path,list);
-            dfs(root.right,path,list);
+            dfs(root.left,sum,path,list,targetSum);
+            dfs(root.right,sum,path,list,targetSum);
         }
         path.remove(path.size()-1);
+        sum-=root.val;
+       
 
     }
 }
