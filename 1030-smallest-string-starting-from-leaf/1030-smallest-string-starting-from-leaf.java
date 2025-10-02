d2a0ae4e045473016a32ce14a92ee172b401bfdc
Chandranshu <rishpandey59@gmail.com>
2025-10-03 01:26:00 +0530

    Time: 4 ms (48.23%), Space: 45.7 MB (10.91%) - LeetHub

--- 1030-smallest-string-starting-from-leaf/1030-smallest-string-starting-from-leaf.java
+++ 1030-smallest-string-starting-from-leaf/1030-smallest-string-starting-from-leaf.java
@@ -23,15 +23,15 @@ class Solution {
     }
     public void dfs(TreeNode root, StringBuilder sb, List<String> temp){
         if(root==null) return;
-        char c=(char) ('a'+root.val);
+        char c=(char) ('a'+root.val);  // numb to char
         int len=sb.length();
         sb.append(c);
         if(root.left==null && root.right==null){
-            temp.add(new StringBuilder(sb).reverse().toString());
+            temp.add(new StringBuilder(sb).reverse().toString()); // always add new sb as if we reverse the original one then it would get reversed for rest of the operations as well
         }else{
               dfs(root.left,sb,temp);
               dfs(root.right,sb,temp);
         }
-        sb.setLength(len);
+        sb.setLength(len); // backtrack, i.e remove the prev one
     }
 }
