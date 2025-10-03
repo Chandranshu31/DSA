ee2f7167f87524d6c46d1491a7a3efdeca886516
Chandranshu <rishpandey59@gmail.com>
2025-10-03 22:23:07 +0530

    Time: 0 ms (100%), Space: 43.4 MB (26.7%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -15,16 +15,29 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root==null){
-            return false;
-        }
+        if(root==null) return false;
+        List<Integer> temp =  new ArrayList<>();
+        dfs(root,0,temp,targetSum);
+        return temp.size()!=0;
+    }
+    public void dfs(TreeNode root,int sum, List<Integer>temp,int targetSum){
+        if(root==null) return;
+       // path.add(root.val);
+        sum+=root.val;
         if(root.left==null && root.right==null){
-            return root.val==targetSum;
+            if(sum==targetSum){
+                temp.add(sum);
+                return;
+            }
+        }else{
+            dfs(root.left,sum,temp,targetSum);
+            dfs(root.right,sum,temp,targetSum);
         }
 
-        int remainingSum=targetSum-root.val;
+       // path.remove(path.size()-1);
+       // sum-=root.val; we dont need to do it because int is primitive and they are call by reference and in the prev level their old or that level value is intact
+       //while we need to remove the path element because they r objects n they re call by value so to every level of recursion
+       // their original ersion is sent and made chnages.
 
-        return hasPathSum(root.left,remainingSum) || hasPathSum(root.right,remainingSum);
-        
     }
 }
