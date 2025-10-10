fb30064772bd430a005131615ccf9f525ee16c7f
Chandranshu <rishpandey59@gmail.com>
2025-10-11 00:58:17 +0530

    Time: 3 ms (6.22%), Space: 44.7 MB (18.58%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -54,3 +54,25 @@ class Solution {
         
     }
 }
+
+/** A SOLN WITHOUT SPACE, for root q.peek would store null at front when root polled
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) return root;
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int level = q.size();
+            while (level-- > 0) {
+                Node curr = q.poll();
+                if (level != 0) curr.next = q.peek();
+                if (curr.left != null) q.add(curr.left);
+                if (curr.right != null) q.add(curr.right);
+            }
+        }
+
+        return root;
+    }
+} */
