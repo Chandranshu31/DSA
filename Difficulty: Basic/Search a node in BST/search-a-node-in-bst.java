53ead597d3838ba173a9b4e3bed782597946d4cc
Chandranshu <rishpandey59@gmail.com>
2025-10-11 01:36:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Search a node in BST/search-a-node-in-bst.java
@@ -0,0 +1,25 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
+*/
+
+class BST {
+    // Function to search a node in BST.
+    boolean search(Node root, int x) {
+        // Your code here
+        if(root==null) return false;
+        if(root.data==x) return true;
+        if(x<root.data){
+            return search(root.left,x);
+        }
+        return search(root.right,x);
+        
+    }
+}
