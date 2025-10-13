bf6f3d76298d4ee40be54a8f4337ccd55b02270d
Chandranshu <rishpandey59@gmail.com>
2025-10-13 12:47:12 +0530

    Time: 6 ms (98.61%), Space: 48 MB (56.87%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,6 +2,27 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> res = new ArrayList<>();
         if(strs.length==0) return res;
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(int i=0;i<strs.length;i++){
+            String curr=strs[i];
+            char arr[]=curr.toCharArray();
+            Arrays.sort(arr);
+            String key= new String(arr);
+
+            map.putIfAbsent(key,new ArrayList<>());
+            map.get(key).add(curr);
+        }
+        res.addAll(map.values());
+        return res;
+        
+    }
+} 
+
+/** ANOTHER VALID SOLUTION
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+        if(strs.length==0) return res;
         HashSet<Integer> groupped = new HashSet<>();
         for(int i=0;i<strs.length;i++){
             String curr=strs[i];
@@ -33,4 +54,4 @@ class Solution {
         for(int f: freq) if(f!=0) return false;
         return true;
     }
-}
+} */
