9f607f8406c9f4bc94a963e95041021f7e0b1bad
Chandranshu <rishpandey59@gmail.com>
2025-10-31 22:38:34 +0530

    Time: 1 ms (43.29%), Space: 53.9 MB (5.06%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.java
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.java
@@ -14,15 +14,12 @@
  * }
  */
 class Solution {
-    int sum=0;
     public int rangeSumBST(TreeNode root, int low, int high) {
-        dfs(root,low,high);
-        return sum;
-    }
-    public void dfs(TreeNode root,int low, int high){
-        if(root==null) return;
+        if(root==null) return 0;
+        int sum=0;
         if(root.val>=low && root.val<=high) sum+=root.val;
-        dfs(root.left,low,high);
-        dfs(root.right,low,high);
+        sum+=rangeSumBST(root.left,low,high);
+        sum+=rangeSumBST(root.right,low,high);
+        return sum;        
     }
 }
