cf399cfced1024138373b87597a0e5cace0bab40
Chandranshu <rishpandey59@gmail.com>
2025-12-10 23:40:54 +0530

    Time: 22 ms (8.86%), Space: 47 MB (16.62%) - LeetHub

---
+++ 0748-shortest-completing-word/0748-shortest-completing-word.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        HashMap<Character,Integer> licMap= new HashMap<>();
+        for(int i=0;i<licensePlate.length();i++){
+            char curr=Character.toLowerCase(licensePlate.charAt(i)); // to handle case insensitive
+            if(Character.isLetter(curr)){
+                licMap.put(curr,licMap.getOrDefault(curr,0)+1);
+            }
+        }
+
+        String ans = null;
+        for (String w : words) {
+            HashMap<Character,Integer> wordMap = new HashMap<>();
+            for (int i = 0; i < w.length(); i++) {
+                char c = Character.toLowerCase(w.charAt(i));
+                if (Character.isLetter(c)) {
+                    wordMap.put(c, wordMap.getOrDefault(c, 0) + 1);
+                }
+            }
+
+            if (hasAllCharactersForSameNumbOfTimes(licMap, wordMap)) {
+                if (ans == null || w.length() < ans.length()) {
+                    ans = w;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public boolean hasAllCharactersForSameNumbOfTimes(HashMap<Character,Integer> licMap, HashMap<Character,Integer> wordMap){
+        for (Character c : licMap.keySet()) {
+            if (!wordMap.containsKey(c)) return false;
+            if (wordMap.get(c) < licMap.get(c)) return false; // must have at least required count
+        }
+        return true;
+    }
+}
