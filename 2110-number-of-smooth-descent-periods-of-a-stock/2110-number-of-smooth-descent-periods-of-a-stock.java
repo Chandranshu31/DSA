7c51c490177453fc9f5dda8abb9e3722621845a1
Chandranshu <rishpandey59@gmail.com>
2025-12-15 23:16:02 +0530

    Time: 2 ms (100%), Space: 84.4 MB (21.48%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.java
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.java
@@ -1,7 +1,7 @@
 class Solution {
     public long getDescentPeriods(int[] prices) {
         long ans = 1;      // first element
-        long streak = 1;  // length of current descent chain
+        long streak = 1;  // length of current chain upto descending
 
         for (int i = 1; i < prices.length; i++) {
             if (prices[i - 1] - prices[i] == 1) {
@@ -14,3 +14,23 @@ class Solution {
         return ans;
     }
 }
+
+/**
+Another soln but it is in O(n^2) 
+class Solution {
+    public long getDescentPeriods(int[] prices) {
+      int n=prices.length;
+      long ans=0;
+      for(int i=0;i<=n-2;i++){
+        int curr=prices[i];
+        int j=i+1;
+        long count=0;
+        while(j<n && prices[j-1]-prices[j]==1){
+            count++;
+            j++;
+        }
+        ans+=count;
+      } 
+      return ans+n; 
+    }
+}*/
