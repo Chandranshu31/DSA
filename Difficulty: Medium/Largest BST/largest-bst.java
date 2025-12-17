eb951505578d4f88b7eeee8976f475d5a0ce6590
Chandranshu <rishpandey59@gmail.com>
2025-12-17 22:23:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest BST/largest-bst.java
+++ Difficulty: Medium/Largest BST/largest-bst.java
@@ -11,44 +11,48 @@
 // }
 
 class Solution {
-    static int maxSize=0;
+    static int maxBST=0;
     static class info{
         boolean isBST;
         int size;
-        int min;
         int max;
-        public info(boolean isBST, int size, int min, int max){
+        int min;
+        public info(boolean isBST, int size, int max, int min){
             this.isBST=isBST;
             this.size=size;
-            this.min=min;
             this.max=max;
+            this.min=min;
         }
     }
-   
 
     // Return the size of the largest sub-tree which is also a BST
     static int largestBst(Node root) {
-        maxSize=0;
+        // Write your code here
+        maxBST=0;
         largestBST(root);
-        return maxSize;
+        return maxBST;
+        
         
     }
-    public static  info largestBST(Node root){
-        if(root==null) return new info(true,0,Integer.MAX_VALUE,Integer.MIN_VALUE);
+    public static info largestBST(Node root){
+        if(root==null) return new info(true,0,Integer.MIN_VALUE,Integer.MAX_VALUE);
+        
         info leftInfo=largestBST(root.left);
-        info rightInfo=largestBST(root.right);
+        info rightInfo= largestBST(root.right);
         
         int size=leftInfo.size+rightInfo.size+1;
-        int min=Math.min(root.data,Math.min(leftInfo.min,rightInfo.min));
         int max=Math.max(root.data,Math.max(leftInfo.max,rightInfo.max));
+        int min=Math.min(root.data,Math.min(leftInfo.min,rightInfo.min));
         
-        if(root.data<=leftInfo.max || root.data>=rightInfo.min){
-            return new info(false,size,min,max);
+        if((root.data<=leftInfo.max) || (root.data>=rightInfo.min)){
+            return new info(false,size,max,min);
         }
+        
         if(leftInfo.isBST && rightInfo.isBST){
-            maxSize=Math.max(size,maxSize);
-            return new info(true,size,min,max);
+            maxBST=Math.max(maxBST,size);
+            return new info(true,size,max,min);
         }
-        return new info(false,size,min,max);
+        return new info(false,size,max,min);
+    
     }
 }
