4e27c79819577534eafc15739162f5d802524310
Chandranshu <rishpandey59@gmail.com>
2025-12-19 00:50:58 +0530

    Time: 2 ms (98.71%), Space: 44.1 MB (80.96%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,34 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
         int n=s.length();
-        if(n==0){
-            return true;
-        }
+        if(n==0) return true;
         int start=0;
-        int end=n-1;
-
+        int end=s.length()-1;
         while(start<end){
             char currFirst=s.charAt(start);
             char currLast=s.charAt(end);
             if(!Character.isLetterOrDigit(currFirst)){
                 start++;
-            }
-            else if(!Character.isLetterOrDigit(currLast)){
+            }else if(!Character.isLetterOrDigit(currLast)){
                 end--;
-            }
-            else{
-                if(Character.toLowerCase(currFirst)!=Character.toLowerCase(currLast)){
+            }else{
+                if(!(Character.toLowerCase(currFirst)==Character.toLowerCase(currLast))){
                     return false;
+
                 }
                 start++;
                 end--;
             }
         }
         return true;
+        
     }
 }
-
-// Idea is very simple, set two pointers at start and end, if the characters at the two pointers are non-alphanumeric just move them 
-// forward and backward respectively
-// then if the chars at both pointers is alphanumeric, convert them to lowercase and compare them, if they are mismatched then simply return false
-// otherwise if matched then move both the pointers
