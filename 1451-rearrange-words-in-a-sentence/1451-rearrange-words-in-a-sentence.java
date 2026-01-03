519bbe554d32804331e30d89bde82fa5b46403c1
Chandranshu <rishpandey59@gmail.com>
2026-01-04 01:38:54 +0530

    Time: 46 ms (20.27%), Space: 47.2 MB (83.83%) - LeetHub

--- 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.java
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.java
@@ -1,14 +1,14 @@
 class Solution {
     public String arrangeWords(String text) {
-        text=text.toLowerCase();
-        String arr[]=text.trim().split("\\s+");
-        Arrays.sort(arr,(a,b)->a.length()-b.length());
+        text=text.toLowerCase(); // convert full string into lowercase
+        String arr[]= text.trim().split("\\s+");
+        Arrays.sort(arr,(a,b)-> a.length()-b.length());// sort in asc order on the basis of length, its a stable sort so if a.length()==b.length(), the relative order is maintained
         StringBuilder sb = new StringBuilder();
-        for(String word:arr){
-            sb.append(word).append(" ");
+        for(String word: arr){
+            sb.append(word).append(" "); // space after every word
         }
+
         sb.setCharAt(0,Character.toUpperCase(sb.charAt(0)));
-        return sb.toString().trim();
-        
+        return sb.toString().trim(); // to remove last added extra space after the last word        
     }
 }
