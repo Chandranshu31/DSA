f060363c0ec24f63266e1c375c2509c4d267509a
Chandranshu <rishpandey59@gmail.com>
2026-02-20 00:52:07 +0530

    Time: 115 ms (68.33%), Space: 242.5 MB (7.49%) - LeetHub

--- 3843-first-element-with-unique-frequency/3843-first-element-with-unique-frequency.java
+++ 3843-first-element-with-unique-frequency/3843-first-element-with-unique-frequency.java
@@ -1,13 +1,15 @@
 class Solution {
     public int firstUniqueFreq(int[] nums) {
-        LinkedHashMap<Integer,Integer> map = new LinkedHashMap<>();
+        LinkedHashMap<Integer,Integer> map = new LinkedHashMap<>(); // count freq of numbers, linked imp to maintain the order
         for(int num:nums){
             map.put(num,map.getOrDefault(num,0)+1);
         }
-        HashMap<Integer,Integer> hMap = new HashMap<>();
+
+        HashMap<Integer,Integer> hMap = new HashMap<>(); // count freq of frequencies
         for(int freq:map.values()){
             hMap.put(freq,hMap.getOrDefault(freq,0)+1);
         }
+        // then interaate on first map to check which first numb has unique freq
         for(int key:map.keySet()){
             int freq=map.get(key);
             if(hMap.get(freq)==1){
@@ -18,3 +20,5 @@ class Solution {
    
     }
 }
+
+// find fereq, then freq of freq, then chck which is the first number whose freq is unique that is its freq's freq is 1
