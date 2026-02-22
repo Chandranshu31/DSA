a49c0d66915c27710b4bfc4dc26c5efda8933ced
Chandranshu <rishpandey59@gmail.com>
2026-02-22 22:54:42 +0530

    Time: 1 ms (99.99%), Space: 47 MB (67.24%) - LeetHub

--- 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.java
+++ 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.java
@@ -2,13 +2,13 @@ class Solution {
     public int scoreDifference(int[] nums) {
         int a=0;// initially active
         int b=0; // initially inactive
-        boolean isActive=true; // for A
+        boolean isActive=true; // for A 
 
         for(int i=0;i<nums.length;i++){
-            if(nums[i]%2!=0){
+            if(nums[i]%2!=0){  // if odd make a inactive
                 isActive=!(isActive);
             }
-            if(isEverySixth(i)){
+            if(isEverySixth(i)){  // same for other condiiton
                 isActive=!(isActive);
             }
             if(isActive)a+=nums[i];
@@ -17,7 +17,7 @@ class Solution {
         return a-b;
 
     }
-    public static boolean isEverySixth(int x) {
+    public static boolean isEverySixth(int x) { // func to check if its the every sixth indx..that is 5,11,17...
     return x >= 5 && (x - 5) % 6 == 0;
 }
 }
